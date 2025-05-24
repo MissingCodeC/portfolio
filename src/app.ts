96923c5325f960c4fb684f296bb5f14680e807ea
MissingCodeC <erfan.baghdadi26@gmail.com>
2025-05-25 01:36:44 +0330

    added the hero section

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 import '../public/styles.css';
 
-alert('kire khar');
+const navbar = document.querySelector('.main-nav__items')! as HTMLUListElement;
+navbar.addEventListener('click', event => {
+    const clickedEl = event.target as HTMLElement;
+    if (clickedEl.tagName === 'UL') {
+        return;
+    }
+
+    const navItem = clickedEl.closest('li') as HTMLLIElement;
+    const currentlyActiveEl = document.querySelector('.main-nav__item--active') as HTMLLIElement;
+    if (navItem === currentlyActiveEl) {
+        return;
+    }
+
+    currentlyActiveEl.classList?.remove('main-nav__item--active');
+    navItem.classList?.add('main-nav__item--active');
+});
